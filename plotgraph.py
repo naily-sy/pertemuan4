1f03f067dae04349c0435baa205964844a0971e4
naily-sy <138029993+naily-sy@users.noreply.github.com>
2023-11-01 08:26:04 +0700

    Update plotgraph.py

--- plotgraph.py
+++ plotgraph.py
@@ -10,9 +10,14 @@ st.subheader('Plot by Bapak Nug')
 nama = st.text_input('Nama', 'Naily', label_visibility='collapsed')
 st.write('Halo ', nama)
 
+f1 = st.number_input('f1= ', value=1)
+f2 = st.number_input('f2= ', value=1)
+st.write('Frekuensi plot sinus adalah ', f1)
+st.write('Frekuensi plot cosinus adalah ', f2)
+
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(x)  # Calculating sin(x) values
-z = np.cos(x)  # Calculating sin(x) values
+y = np.sin(f1*x)  # Calculating sin(x) values
+z = np.cos(f2*x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -33,7 +38,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.caption('Sin')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.sin(x)  # Calculating sin(x) values
+    y = np.sin(f1*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -48,7 +53,7 @@ with col1:
 with col2:
     st.caption('Cos')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.cos(x)  # Calculating sin(x) values
+    y = np.cos(f2*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='cos(x)', color='g')  # Plotting cos(x) curve
