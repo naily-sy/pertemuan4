38b6fbe3a6a5bada18704abeef6cb90b40f6d8e0
naily-sy <138029993+naily-sy@users.noreply.github.com>
2023-12-18 13:23:43 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -14,59 +14,6 @@ option = st.selectbox(
     ('C','F', 'R', 'K'))
 
 f1 = st.number_input('f1= ', value=1)
-f2 = st.number_input('f2= ', value=1)
-st.write('Frekuensi plot sinus adalah ', f1)
-st.write('Frekuensi plot cosinus adalah ', f2)
-
-x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(f1*x)  # Calculating sin(x) values
-z = np.cos(f2*x)  # Calculating sin(x) values
- 
-fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
-ax.plot(x, z, label='cos(x)', color='g')  # Plotting sin(x) curve
-ax.set_ylabel("")
-ax.set_xlabel("x")
-ax.tick_params(axis='y', labelsize=20)
-ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
-ax.tick_params(axis='x', labelsize=15)
-
-st.pyplot(fig)
- 
-# subheader
-st.subheader("Plot Sin & Cos")
- 
-col1, col2 = st.columns(2)
- 
-with col1:
-    st.caption('Sin')
-    x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.sin(f1*x)  # Calculating sin(x) values
- 
-    fig, ax = plt.subplots(figsize=(16, 8))
-    ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
-    ax.set_ylabel("Sin x")
-    ax.set_xlabel("x")
-    ax.tick_params(axis='y', labelsize=20)
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
-    ax.tick_params(axis='x', labelsize=15)
-
-    st.pyplot(fig)
- 
-with col2:
-    st.caption('Cos')
-    x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.cos(f2*x)  # Calculating sin(x) values
- 
-    fig, ax = plt.subplots(figsize=(16, 8))
-    ax.plot(x, y, label='cos(x)', color='g')  # Plotting cos(x) curve
-    ax.set_ylabel("Cos x")
-    ax.set_xlabel("x")
-    ax.tick_params(axis='y', labelsize=20)
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
-    ax.tick_params(axis='x', labelsize=15)
- 
-    st.pyplot(fig)  
  
 st.caption('source: Nugroho Adi Pramono nugroho.xyz')
 
