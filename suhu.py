d7a2112baf6281be4b4ea0f2f98f249c1b286c5c
naily-sy <138029993+naily-sy@users.noreply.github.com>
2023-12-19 11:58:47 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -5,7 +5,7 @@ import streamlit as st
  
 # Header
 st.header('hehe :sparkles:')
-st.subheader('Plot by Bapak Nug')
+st.subheader('Konversi Suhu')
 
 nama = st.text_input('Nama', 'Naily', label_visibility='collapsed')
 st.write('Halo ', nama)
